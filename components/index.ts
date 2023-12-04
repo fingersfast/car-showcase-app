a53c1ff3667417688a7d34bcd6053dd3044e6f65
Himanshu Yengal <Himanshu.yengal@gmail.com>
2023-12-04 11:57:13 +0530

    added carCard and carDetails files

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,6 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import CustomButton from "./CustomButton";
 import Hero from "./Hero";
+import CustomFilter from "./CustomFilter";
 
-export { Navbar, Footer, CustomButton, Hero };
+export { Navbar, Footer, CustomButton, Hero, CustomFilter };
